48f6cb32a7d1361704fa09a4ee165945d06df8c0
AllenCoder <AllenCoder@126.com>
2017-07-13 19:39:45 +0800

    Initial commit

---
+++ tests.py
@@ -0,0 +1,14 @@
+import jieba.analyse
+
+path = 'test.txt'
+
+fp = open(path,'r',encoding='utf-8')
+content = fp.read()
+try:
+    # jieba.analyse.set_stop_words('停用词表路径')
+    tags = jieba.analyse.extract_tags(content, topK=100, withWeight=True)
+    for item in tags:
+        print(item[0]+'\t'+str(int(item[1]*1000)))
+
+finally:
+    fp.close()

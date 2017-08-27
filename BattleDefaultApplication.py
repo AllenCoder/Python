8429c64ea113e758cae1aba6fd0478e22ff36e5e
AllenCoder <AllenCoder@126.com>
2017-08-27 22:25:15 +0800

    测试上传body 的json

--- BattleDefaultApplication.py
+++ BattleDefaultApplication.py
@@ -1,4 +1,7 @@
-from bottle import Bottle, run,request,static_file
+import urllib
+
+from bottle import Bottle, run, request,response, static_file
+import json
 
 app = Bottle()
 
@@ -13,8 +16,21 @@ def word():
     return "world"
 
 
+@app.route("/activities", method='POST')
+def activities():
+    body = request.body.read()
+    # parsedBody = urllib.unquote(body).decode('utf8')
+    print("收到上传请求", body)
+    from json import dumps
+    rv = {"code": 1, "message": "Test Item 1"}
+    response.content_type = 'application/json'
+
+    return  dumps(rv)
+
+
 @app.route('/login')
 def login():
+    print("打印", request.body)
     return '''
         <form action="/login" method="post">
             Username: <input name="username" type="text" />
@@ -25,7 +41,7 @@ def login():
 
 
 def check_login(username, password):
-    print(username,password)
+    print(username, password)
     return True
 
 
@@ -33,19 +49,27 @@ def check_login(username, password):
 def do_login():
     username = request.forms.get('username')
     password = request.forms.get('password')
-    header= request.headers.get("header")
-    cookies =request.cookies.get('counter',"0")
+    header = request.headers.get("header")
+    cookies = request.cookies.get('counter', "0")
     form_id = request.query.id
     if check_login(username, password):
         return "<p>Your login information was correct.</p>"
     else:
         return "<p>Login failed.</p>"
+
+
 # 静态文件
 @app.route('/file')
 def file():
-    return static_file("170717007.xml",root="")
+    return static_file("170717007.xml", root="")
+
+
 # 下载文件
 @app.route('/download/<filename:path>')
 def download(filename):
     return static_file(filename, root='', download=filename)
-run(app, host="localhost", port=8089)
+
+
+run(app, host="192.168.1.102", port=8089)
+
+# run(app, host="localhost", port=8089)
